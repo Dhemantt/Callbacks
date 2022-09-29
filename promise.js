1f7708102ce97276924aa2a05a2c494811859ceb
Dhemantt <dixithemant02>
2022-09-29 16:44:38 +0530

    promise with delete post

--- promise.js
+++ promise.js
@@ -30,7 +30,40 @@ function createpost(post) {
     },2000)
 }
 
+function deletepost() {
+    debugger
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (posts.length > 0) {
+                const deletelement = posts.pop();
+                resolve(deletelement);
+            }
+            else {
+                reject('Array is empty now')
+            }
+        },1000)
+    })
+}
+
 createpost({ title: 'post three', body: 'this is post three' })
-    .then(getpost)
+    .then(() => {
+        getpost()
+        deletepost().then((deletelement) => {
+            console.log(deletelement)
+            getpost();
+            deletepost().then(() => {
+                getpost();
+                deletepost().then(() => {
+                    getpost();
+                    deletepost().then(() => {
+                    }).catch((err) => {
+                        console.log('inside catch block', err)
+                    })
+                }).catch((err) => {console.log('3rd deletepost')})
+            }).catch((err) => {console.log('2nd deletepost')})
+        }).catch((err) => {console.log('1st deletepost')})
+    }).catch((err) => {console.log('1st createPost')})
     .catch(error);
+
+
 
